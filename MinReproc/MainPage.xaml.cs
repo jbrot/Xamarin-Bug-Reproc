faf842a581938934036f3361392fc54c496eb9dd
Joshua Brot <jbrot@umich.edu>
2020-02-12 21:11:06 -0500

    Configured for new bug.
    Signed-off-by: Joshua Brot <jbrot@umich.edu>

--- MinReproc/MainPage.xaml.cs
+++ MinReproc/MainPage.xaml.cs
@@ -11,19 +11,11 @@ namespace MinReproc
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible (false)]
-    public partial class MainPage : MasterDetailPage
+    public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        void Button_Pressed(object sender, EventArgs e)
-        {
-            DetailCP.Content = new Label
-            {
-                Text = "New Contents"
-            };
         }
     }
 }
